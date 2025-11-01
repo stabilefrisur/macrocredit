b774b1bfa71ce2250e4dcd0bf329dfca31ba32fd
Stabile Frisur <26568863+stabilefrisur@users.noreply.github.com>
2025-11-01 18:13:15 +0000

    refactor: Use dynamic versioning via importlib.metadata

--- src/aponyx/__init__.py
+++ src/aponyx/__init__.py
@@ -4,7 +4,9 @@ Aponyx - Systematic Macro Credit Strategy Framework.
 A modular Python framework for developing and backtesting systematic credit strategies.
 """
 
-__version__ = "0.1.0"
+from importlib.metadata import version
+
+__version__ = version("aponyx")
 
 
 def hello() -> str:
